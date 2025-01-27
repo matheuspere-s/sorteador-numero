82c748d6719dc069bb4dc7d54abbe4f264e2475a
Matheus Peres <matheuscarvalhocontact@gmail.com>
2025-01-26 21:13:42 -0300

    Criado função sortear recuperando valores do html; criado loop for para gerar os números aleátorios de acordo com os parâmetros informados e outro loop while para verificar se já possui algum número na lista de número sorteados e não repetir.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 function gerarNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    let listaNumeros = [];
+    let resultado = document.getElementById("resultado");
+
+    for(var i = 0; i < quantidade; i++){
+        numero = gerarNumeroAleatorio(de, ate);
+
+        while(listaNumeros.includes(numero)){
+            numero = gerarNumeroAleatorio(de, ate);
+        }
+        listaNumeros.push(numero);
+    }
+
+    resultado.textContent = `Números sorteados: ${listaNumeros}`;
+}
